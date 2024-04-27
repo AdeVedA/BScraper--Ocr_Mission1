8761beda2bc0a8b2c72d08acab6f1903b901281a
Ju_M <julienmarc@free.fr>
2024-04-27 22:54:21 +0200

    Reac:/Users/DevPy/_DEV/BScraper_m1/env/Scripts/python.exe c:/Users/DevPy/_DEV/BScraper_m1/BScraper.py

--- BScraper.py
+++ BScraper.py
@@ -6,7 +6,8 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-def book_categories(url_home): # utilisée 1fois, on recupère les adresses mères de chaque catégorie
+def book_categories(url_home): 
+# utilisée 1fois, on recupère les adresses mères de chaque catégorie
 
     response = requests.get(url_home)
     if response.ok:
@@ -19,7 +20,12 @@ def book_categories(url_home): # utilisée 1fois, on recupère les adresses mèr
     else:
         print("Veuillez vérifier la connexion ou l'installation de l'environnement, code erreur : ", reponse)
 
-def pages_categorie(url_categorie): #on récupère les liens de chaque livre dans toutes les pages d'une catégorie
+def livres_pages_categorie(url_categorie):
+
+    # input la page d'accueil de la catégorie, 
+    # on récupère les liens de chaque livre 
+    # dans TOUTES les pages possibles d'une catégorie
+
     i = 1
     liens_livres_cat = []
     while True:
@@ -27,10 +33,11 @@ def pages_categorie(url_categorie): #on récupère les liens de chaque livre dan
         soup = BeautifulSoup(response.text, 'lxml')
         # on récupère le bouts d'url spécifique à chaque livre et recompose/ajoute
         # les adresses des livres de la catégorie dans liens_livres_cat
-        h3s = soup.find_all('h3')
-        for h3 in h3s:
-            str_lien_livre = h3.find('a')['href']
-            liens_livres_cat.append('http://books.toscrape.com/catalogue/' + str_lien_livre[6:].replace("../", ""))
+
+        # on recrée tous les liens de catégorie à partir de balise html
+        for h3 in soup.find_all('h3'):
+            liens_livres_cat.append('http://books.toscrape.com/catalogue/' + h3.find('a')['href'][6:].replace("../", ""))
+        # et tant que le bouton 'next' existe,
         try:
             next_btn = soup.find('li', {'class':'next'}).find('a').text
             if next_btn is None:
@@ -38,74 +45,94 @@ def pages_categorie(url_categorie): #on récupère les liens de chaque livre dan
                 print(f"urls des livres de {url_categorie} page {i} récupérées")
         except:
             return(liens_livres_cat)
-        #on itère le numéro de page tant que le bouton 'next' existe
+        # on itère sur le numéro de page et on recommence
         i += 1
-        url_categorie = (url_categorie[:-10] + 'page-' + str(i) + '.html')    
+        url_categorie = (url_categorie[:-10] + 'page-' + str(i) + '.html')
 
-def infos_livres_cat_csv(liens_livres_cat): # pour une catégorie on crée un repertoire on y met les datas dans un csv et les images en .jpg
-    # on prépare le répertoire d'accueil de la catégorie
+def dossier_cat_csv(url_categorie):
+    
+    # on crée un repertoire pour une catégorie
+# 1. input = url-categorie / url tree parsing / pour recréer un nom ordonné de répertoire "x-catégorie"
     url_cat_scrap = urlparse(url_categorie)
-    parts_categorie = url_cat_scrap.path.strip('/').split('/')[3].split('_')
-    categorie_ordre = (parts_categorie[1] + '_' + parts_categorie[0])
-    print(f'le dossier portera le nom de {categorie_ordre}')
+    parts_categorie_x = url_cat_scrap.path.strip('/').split('/')[3].split('_')
+    x_categorie = (parts_categorie_x[1] + '_' + parts_categorie_x[0])
+    print(f'le dossier portera le nom de {x_categorie}')
+
+    # on se met de côté la variable dir_cat pour la destination des fichiers de la categorie
+    dir_cat = os.path.join(catalog_dir, x_categorie)
     # on crée le dossier de catégorie dans lequel seront créés téléchargés les csv jpg de cette catégorie
-    os.makedirs(os.path.join(os.getcwd(), os.path.join('Catalogue' + f'_{year}.{month}.{day}_v{i}'), categorie_ordre), exist_ok=False)
-    # on se met de côté la variable dossier_categorie pour la destination des fichiers
-    dir_cat = os.path.join(os.getcwd(), os.path.join('Catalogue' + f'_{year}.{month}.{day}_v{i}'), categorie_ordre)
-            
-    for url_livre in liens_livres_cat:
-        reponse = requests.get(url_livre)
-        soup = BeautifulSoup(reponse.text.encode('latin1').decode('utf-8'), 'lxml')
-        # datas à scraper par livre :
-        url_livre = url_livre + " "
-        table_tds = soup.findAll('td') #tableau avec 4datas en lignes[0:6] UPC,...,prix,prix+taxe,...,dispos
-        prod_disponibles = table_tds[5].text
-        titre_h = soup.find('h1')
-        prod_description = soup.find('article', {'class': 'product_page'}).findAllNext('p')[3].contents[0]
-        categorie = soup.find('ul', {'class': 'breadcrumb'}).findAll('li')[2].find('a')
-        etoiles = soup.find('div', {'class': 'col-sm-6 product_main'}).findAll('p')[2].attrs['class']
-        img_couverture = soup.find('div', {'class': 'item active'}).find('img').attrs['src']
-        
-        datas = [url_livre, table_tds[0].text, titre_h.text, table_tds[3].text[1:],
-         table_tds[2].text[1:], prod_disponibles[10:-11], str(prod_description[:-8]), categorie.text,
-         etoiles[1], 'http://books.toscrape.com/' + str(img_couverture)[6:]]
-        champs_datas = ['product_page_url', 'universal_product_code(UPC)', 'title',
-         'price_including_tax', 'price_excluding_tax', 'number_available', 'product_description',
-         'category', 'review_rating', 'image_url']
-        # datas[0] = url_livre.append(" ")
-        # A la première écriture du fichier "categoroie".csv, on inscrit les champs_datas en header et les datas du 1er livre en dessous
+    os.makedirs(dir_cat, exist_ok=False)
+    return(dir_cat)
+
+def getdatas_livre (url_livre):
+
+    # on récupère les datas des livres
+# 2. on crée deux listes : catégories et datas et une adresse image          
+    reponse = requests.get(url_livre)
+    soup = BeautifulSoup(reponse.text.encode('latin1').decode('utf-8'), 'lxml')
+# datas à scraper par livre sur sa page url_livre:
+    url_livre = url_livre + " "
+    # tableau avec 4datas en lignes[0:6] UPC,...,prix,prix+taxe,...,dispos
+    table_tds = soup.findAll('td') 
+    prod_dispos = table_tds[5].text[10:-11]
+    titre_h = soup.find('h1').text
+    prod_description = soup.find('article', {'class': 'product_page'}).findAllNext('p')[3].contents[0][:-8]
+    categorie = soup.find('ul', {'class': 'breadcrumb'}).findAll('li')[2].find('a').text
+    etoiles = soup.find('div', {'class': 'col-sm-6 product_main'}).findAll('p')[2].attrs['class'][1]
+    img_couverture = soup.find('div', {'class': 'item active'}).find('img').attrs['src'][6:]
+# deux listes de datas : les champs communs et les datas d'un livre 
+    champs_datas = ['product_page_url', 'universal_product_code(UPC)', 'title','price_including_tax',
+        'price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+    datas = [url_livre, table_tds[0].text, titre_h, table_tds[3].text[1:],
+        table_tds[2].text[1:], prod_dispos, str(prod_description), categorie,
+        etoiles, 'http://books.toscrape.com/' + str(img_couverture)]
+    return champs_datas, datas
+
+def ecrire_csv_img(champs_datas,datas): # datas dans un csv & img dans le dossier        
+
+# 3. Si c'est la première écriture du fichier de cette 'catégorie'.csv
+    # on inscrit les champs_datas en header et les datas du 1er livre en dessous
         if not os.path.exists(os.path.join(dir_cat, f"{datas[7]}.csv")):
             with open(os.path.join(dir_cat, f"{datas[7]}.csv"), 'w', encoding='utf8', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(champs_datas)
-        # si le fichier csv existe déjà (livre n° >= 2), on ajoute les données en dessous dans le csv
+    # si le fichier csv existe déjà (livre n° >= 2), on ajoute les données en dessous dans le csv
         with open(os.path.join(dir_cat, f"{datas[7]}.csv"), 'a', encoding='utf8', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(datas)
-        print(f"datas de '{datas[2]}' de la catégorie '{datas[7]}' écrites avec succès dans {datas[7]}.csv")
+        print(f"datas de '{datas[2]}' écrites avec succès dans {datas[7]}.csv")
+
+# 4. on va sauvegarder l'image nommée à partir du 
+# titre nettoyé de caractères interdits pour filename
         nom_fichier_image = datas[2]
         for char in ':/*?"<>|':
             nom_fichier_image = nom_fichier_image.replace(char, '')
         urllib.request.urlretrieve(f"{datas[-1]}", os.path.join(dir_cat, f"{nom_fichier_image}.jpg"))
-        print(f"image de {datas[2]} sauvegardée avec succès dans {nom_fichier_image}.jpg")
+        print(f"image sauvegardée avec succès dans {nom_fichier_image}.jpg")
 
-url_home = 'http://books.toscrape.com/index.html'
-urls_categories = book_categories(url_home)
-# on est quand ?
+
+# on prend la date pour créer un dossier catalogue par requête v0 v1 v2... (Catalogue_2024.4.27_v0)
 today = date.today()
 year, month, day = map(str, (today.year, today.month, today.day))
+#initialiser i pour incrémenter si la version i existe
 i = 0
-# dossier_courant = os.getcwd(), on ajoute Catalogue et AnnéeMoisJourVersion{i}
-# on cherche le prochain i dispo à l'écriture du dossier de sauvegarde par run du programme
+# catalog_dir = os.getcwd(), on ajoute 'Catalogue'_Année.Mois.Jour_v{i} : 
+# on cherche le prochain i dispo à l'écriture du dossier catalog_dir
 while os.path.exists(os.path.join(os.getcwd(), os.path.join('Catalogue' + f'_{year}.{month}.{day}_v{i}'))):
     i += 1
-# on créer le dossier pour ce téléchargement
-os.makedirs(os.path.join(os.getcwd(), os.path.join('Catalogue' + f'_{year}.{month}.{day}_v{i}')), exist_ok=False)
-# on travaillera dans catalog_dir pour toute la session
 catalog_dir = os.path.join(os.getcwd(), os.path.join('Catalogue' + f'_{year}.{month}.{day}_v{i}'))
+# on créer le dossier pour ce téléchargement, on travaillera dans catalog_dir pour toute la session
+os.makedirs(catalog_dir, exist_ok=False)
 print(f'vos répertoires et fichiers seront dans l\'arborescence du répertoire {catalog_dir}\\')
 
+url_home = 'http://books.toscrape.com/index.html'
+urls_categories = book_categories(url_home) # utilisée 1fois, 
+# on a recupéré les urls mères de chaque catégorie
+
 for url_categorie in urls_categories[1:]:
-    liens_livres_cat = pages_categorie(url_categorie)
-    infos_livres_cat_csv(liens_livres_cat)
+    liens_livres_cat = livres_pages_categorie(url_categorie)
+    dir_cat = dossier_cat_csv(url_categorie)
+    for url_livre in liens_livres_cat:
+        champs_datas, datas = getdatas_livre(url_livre)
+        ecrire_csv_img(champs_datas, datas)
     print(f'la/les pages {url_categorie} ont été sauvegardées dans un csv avec les images des livres')
--- readme.md
+++ readme.md
@@ -2,23 +2,32 @@
 ------------------------------------------
 ## - Scraping de librairie en ligne "à la BeautifulSoup" -
 
-#### "Un Scraper sachant scraper sans Scrapy doit BeautifulSouper sans peur ni sans sampler..."
+##### "Un Scraper sachant scraper sans Scrapy doit BeautifulSouper sans peur ni sans sampler..."
 ------------------------------------------
-Ce repo herberge un programme de scraping réalisé sur demande pour récupérer des datas sur les livres & catégories d'un site de vente en ligne de livres.
-Ces datas sont structurées par catégorie de livre, sauvegardées en fichier .csv avec "informations" par livre,
+BScraper est un programme de scraping récupérant des datas sur un site de vente en ligne de livres http://books.toscrape.com/ 
+Je crée un dossier catalogue du jour,
+télécharge les liens des catégories,
+itère pour chaque catégorie :
+
+-création d'un dossier du nom réordoné de la catégorie 
+-on itère pour chaque livre
+--récupèration des datas des livres
+--ajout des datas dans le csv de la catégorie 
+--sauvegarde de l'image du livre en .jpg
+
+Ces datas sont structurées par catégorie de livres, sauvegardées en fichier .csv classées par livre et champs de data
 et chaque image de livre est téléchargée en fichier .jpg nommé selon son titre.
 Les informations sur l'arborescence d'écriture des données figurent en IV
 
 ------------------------------------------
 
-## I - Installation windows (si python 3.x n'est pas installé, commencez par l'annexe A)
+## I - Setup  windows (si python 3.x n'est pas installé, commencez par l'annexe A)
 
   #### A- Créez un repertoire pour le programme
 dans votre explorateur windows :
 créez un répertoire pour le programme où vous le souhaitez (CTRL+MAJ+N) **nommez-le**
-vous pouvez l'appeler BScraper.
-(ex. : BScraper dans d:\chemin\de\mon\dossier\BScraper)
-double-cliquez sur le répertoire créé pour vous trouver dedans.
+ex. : vous pouvez l'appeler BScraper dans d:\chemin\vers\mon\dossier\BScraper
+double-cliquez sur le répertoire créé pour être dedans.
 
   #### B- lancez l'interpréteur de commande windows
 clic gauche dans la barre d'adresse de l'explorateur, écrivez **"cmd"** et appuyez sur **"entrée"** :
@@ -30,7 +39,7 @@ dans l'invite de commande, écrivez:
 
 	git clone https://github.com/AdeVedA/OCRmission1.git
  
-## II - Installation d'un environnement virtuel et lancement du programme
+## II - Installation d'un environnement virtuel
 
   #### - pour installer un environnement virtuel dans un dossier 'env' du projet, dans l'invite de commande, tapez :
 	
@@ -55,7 +64,7 @@ dans l'invite de commande, écrivez:
    les données sont toutes rassemblées et créées (en .csv) ou téléchargées (en .jpg) 
 dans un nouveau répertoire versionné à chaque lancement et une architecture de répertoire par catégorie ensuite :
 
-         racine/
+      racine/
       	    |---Catalogue_année.mois.jour_v0/
       	    |                               |----2_travel/
       	    |                               |            |-----travel.csv
@@ -66,8 +75,9 @@ dans un nouveau répertoire versionné à chaque lancement et une architecture d
       	    |                               |----x_etc/
       	    |                               L----51_etc/
       	    L---Catalogue_année.mois.jour_v1/ ...
-------------------------------------------
-# Annexe A = installation de python
+
+
+***** Annexe A = installation de python
 
 Si vous n'avez pas installé Python, téléchargez-la dernière version sur **"https://www.python.org/downloads/"** et installez-le en vous assurant que **"Add to PATH"** est coché (laissez les autres choix par défaut)
 	
